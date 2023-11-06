f390adab2607306abdb12561261f235a65418226
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-11-06 11:56:09 -0800

    got the names rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default async function Home() {
+	// I'm making a fetch call here since i'm using async in the function i have to await for the fetch response
+	const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+
+	// the res.ok is to make sure that the status is anything other then 200 then it will throw an error
+	if (!res.ok) {
+		throw new Error('Something went wrong');
+	}
+	const data = await res.json();
+	console.log(data, 'DATA');
 	return (
 		<main>
-			<h1>HELLO</h1>
+			{data.results.map((pokemon) => (
+				<p key={pokemon.name}>{pokemon.name}</p>
+			))}
 		</main>
 	);
 }
