379913cde173f54b19465aadcd874705b8c8841a
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-11-21 09:54:59 -0800

    worked with chris

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import getPokemon from '../lib/getPokemon';
-import Image from 'next/image';
-import { notFound } from 'next/navigation';
-
 export default async function Home() {
-	const data = await getPokemon('pikachu');
-
-	if (!data) {
-		return notFound();
-	}
-
 	return (
 		<main>
-			<Image
-				src={data.sprites.front_default}
-				alt={data.name}
-				width={200}
-				height={200}
-			/>
-			<h1>{data.name}</h1>
+			<h1>Add Form here</h1>
+			<h2>Display Here</h2>
 		</main>
 	);
 }
 
-//HOW TO handle errors in app router HOMEWORK
-// https://nextjs.org/docs/advanced-features/custom-error-page
 
-//https://nextjs.org/docs/app/building-your-application/routing
+// December Lesson Context Light and Dark Mode
