6b12e084b04adcc930211003e4ac414dc75a71a8
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2023-12-11 17:37:20 -0800

    using formData now

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,98 @@
 'use client';
 
 export default function Home() {
-	if (typeof document !== 'undefined') {
-		// Step 1: User Inputs a Pokemon Name
-		const formElement = document.getElementById('form') as HTMLFormElement;
+	// if (typeof document !== 'undefined') {
+	// 	// Step 1: User Inputs a Pokemon Name
+	// 	const formElement = document.getElementById('form') as HTMLFormElement;
 
-		if (formElement) {
-			// Step 2: Add an eventlistener to "listen" for a submit from the form
+	// 	if (formElement) {
+	// 		// Step 2: Add an eventlistener to "listen" for a submit from the form
 
-			formElement.addEventListener('submit', async (event) => {
-				event.preventDefault();
-				// Step 3: Capture the user input value from a form and assign it to a variable variable
-				const pokemonName = (
-					document.getElementById('pokemonName') as HTMLInputElement
-				).value.toLowerCase();
+	// 		formElement.addEventListener('submit', async (event) => {
+	// 			event.preventDefault();
+	// 			// Step 3: Capture the user input value from a form and assign it to a variable variable
+	// 			const pokemonName = (
+	// 				document.getElementById('pokemonName') as HTMLInputElement
+	// 			).value.toLowerCase();
 
-				try {
-					// Step 5: Send a fetch POST Call to the backend api point /api/pokemons which will then trigger a fetch to the pokemon api
-					// go to the route.tsx to continue the lesson
-					const res = await fetch('/api/pokemons', {
-						method: 'POST',
-						body: JSON.stringify({ name: pokemonName }),
+	// 			try {
+	// 				// Step 5: Send a fetch POST Call to the backend api point /api/pokemons which will then trigger a fetch to the pokemon api
+	// 				// go to the route.tsx to continue the lesson
+	// 				const res = await fetch('/api/pokemons', {
+	// 					method: 'POST',
+	// 					body: JSON.stringify({ name: pokemonName }),
 
-						headers: {
-							'Content-Type': 'application/json',
-						},
-					});
+	// 					headers: {
+	// 						'Content-Type': 'application/json',
+	// 					},
+	// 				});
 
-					// Step 11: we are now back from the backend, api and if the res is NOT okay, then we shall throw an error and the http status
-					if (!res.ok) {
-						throw new Error(`HTTP error! Status: ${res.status}`);
-					}
+	// 				// Step 11: we are now back from the backend, api and if the res is NOT okay, then we shall throw an error and the http status
+	// 				if (!res.ok) {
+	// 					throw new Error(`HTTP error! Status: ${res.status}`);
+	// 				}
 
-					// Step 12: We are also in an async function, and we are awaiting the response from the backend to be OK or status 200
-					const responseData = await res.json();
+	// 				// Step 12: We are also in an async function, and we are awaiting the response from the backend to be OK or status 200
+	// 				const responseData = await res.json();
 
-					// Step 13: a checker to make sure we have a name within the response Object
-					if (responseData.name) {
-						// Step 14: if the object has a name, then we shall redirect the user to the pokemon page
-						const redirectUrl = `/pokemons/${responseData.name}`;
-						window.location.href = redirectUrl;
-					} else {
-						console.error('Invalid response data');
-					}
-				} catch {
-					new Error('Pokemon Not Valid');
-				}
+	// 				// Step 13: a checker to make sure we have a name within the response Object
+	// 				if (responseData.name) {
+	// 					// Step 14: if the object has a name, then we shall redirect the user to the pokemon page
+	// 					const redirectUrl = `/pokemons/${responseData.name}`;
+	// 					window.location.href = redirectUrl;
+	// 				} else {
+	// 					console.error('Invalid response data');
+	// 				}
+	// 			} catch {
+	// 				new Error('Pokemon Not Valid');
+	// 			}
+	// 		});
+	// 	}
+	// }
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		const pokemonName = formData.get('pokemonName');
+		formData.append('pokemonName', pokemonName);
+
+		try {
+			console.log('pokemonNameFrontend', pokemonName);
+			const res = await fetch('/api/pokemons', {
+				method: 'POST',
+				body: formData,
+				// headers: {
+				// 	'Content-Type': 'application/json',
+				// },
 			});
+
+			// Step 11: we are now back from the backend, api and if the res is NOT okay, then we shall throw an error and the http status
+			if (!res.ok) {
+				throw new Error(`HTTP error! Status: ${res.status}`);
+			}
+
+			// Step 12: We are also in an async function, and we are awaiting the response from the backend to be OK or status 200
+			const responseData = await res.json();
+
+			// Step 13: a checker to make sure we have a name within the response Object
+			if (responseData.pokemonData.name) {
+				// Step 14: if the object has a name, then we shall redirect the user to the pokemon page
+				const redirectUrl = `/pokemons/${responseData.pokemonData.name}`;
+				window.location.href = redirectUrl;
+			} else {
+				console.error('Invalid response data');
+			}
+		} catch {
+			new Error('Pokemon Not Valid');
 		}
-	}
+	};
 
 	return (
 		<main className="container mx-auto">
 			<form
 				id="form"
 				className="max-w-sm container mx-auto h-screen w-screen flex flex-col justify-center"
+				onSubmit={onSubmit}
 			>
 				<div className="md:flex md:items-center mb-6">
 					<div className="md:w-1/3">
