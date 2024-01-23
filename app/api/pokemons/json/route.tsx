8a5b7e34eb12a5e19629314636894752cda25621
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2024-01-23 08:57:33 -0800

    Lesson with Chris

--- app/api/pokemons/json/route.tsx
+++ app/api/pokemons/json/route.tsx
@@ -5,11 +5,19 @@ export async function GET(request: NextRequest) {
 		const searchParams = request.nextUrl.searchParams;
 		const query = searchParams.get('pokemon');
 		const queryString = query?.toString().toLowerCase();
-		const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${queryString}`);
 
-		const pokemonData = await pokemonResponse.json();
+		try {
+			const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${queryString}`);
+			// Check if pokemon is okay when fetching data
+			if (!pokemonResponse.ok) {
+				throw new Error(`HTTP error! Status: ${pokemonResponse.status}`);
+			}
+			const pokemonData = await pokemonResponse.json();
 
-		return Response.json({ pokemonData });
+			return Response.json({ pokemonData });
+		} catch (error) {
+			throw error;
+		}
 	} catch (error) {
 		console.error('Error:', error);
 		return new NextResponse('Something went wrong', { status: 500 });
