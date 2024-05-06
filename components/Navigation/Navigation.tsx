265235b5c7f7bdd46c42021b29f3abf4fa3ce620
Nikki Sato <54095031+nikkisato@users.noreply.github.com>
2024-05-06 15:25:45 -0700

    Commenting out Todo

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -7,7 +7,10 @@ export default function Navigation({ title, className }: Props) {
 	return (
 		<>
 			<h1 className={className}>{title}</h1>
-			<nav className="flex justify-evenly">
+			<nav
+				id="navigation"
+				className="flex justify-evenly"
+			>
 				<ul className="flex justify-evenly mx-2">
 					<li className="mx-2">
 						<a
